ef4e1425931045b5003be93c851379d6154cf7a5
AK <akashkumarrai210@gmail.com>
2023-08-07 23:56:29 +0530

    change

---
+++ src/components/Editor.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+import "codemirror/lib/codemirror.css"
+import "codemirror/theme/material.css"
+import "codemirror/mode/xml/xml"
+import "codemirror/mode/css/css"
+import 'codemirror/mode/javascript/javascript'
+import {Controlled as ControlledEditor} from 'react-codemirror2'
+
+export default function Editor(props) {
+    const {name, lang, value, onChange, imgSRC} = props
+
+    const [open, setOpen] = useState(true)
+
+    function handleChange(editor, data, value) {
+        onChange(value)
+    }
+
+    return (
+        <div id={name} className={`editor-card ${open ? '': 'collapsed'}`}>
+            <div className='editor-title'>
+                <img src={imgSRC} />
+                {name}
+                <button onClick={()=>setOpen(prevOpen => !prevOpen)}>
+                    <img src={`${open?'img/arrows-fullscreen.svg':'img/fullscreen-exit.svg'}`} />
+                </button>
+            </div>
+            <ControlledEditor
+                onBeforeChange={handleChange}
+                value={value}
+                className='editor-body'
+                options={{
+                    mode: lang,
+                    lineNumbers: true,
+                    lineWrapping: true,
+                    theme: "material",
+                    lint: true,
+                    // matchTagPairs: true,
+                }}
+            />
+        </div>
+    )
+}

fff152a66e3ed471395140c1e25498d7d19c4aed
AK <akashkumarrai210@gmail.com>
2023-08-13 13:10:23 +0530

    Updates

---
+++ precache-manifest.d5eebd2a0d97723552377cd8045e1f24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "087c6d50f63d4837d85c",
+    "url": "/CodePen-Clone/static/css/main.b8253c78.chunk.css"
+  },
+  {
+    "revision": "087c6d50f63d4837d85c",
+    "url": "/CodePen-Clone/static/js/main.087c6d50.chunk.js"
+  },
+  {
+    "revision": "4f0b4039b9a5b91eb041",
+    "url": "/CodePen-Clone/static/css/1.45457d20.chunk.css"
+  },
+  {
+    "revision": "4f0b4039b9a5b91eb041",
+    "url": "/CodePen-Clone/static/js/1.4f0b4039.chunk.js"
+  },
+  {
+    "revision": "1fdf6063bebeecd48365",
+    "url": "/CodePen-Clone/static/js/runtime~main.1fdf6063.js"
+  },
+  {
+    "revision": "4c44ef044907420788dd2c71faf85048",
+    "url": "/CodePen-Clone/index.html"
+  }
+];
